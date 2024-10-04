32091dd53c317b2321bc2685d37d01daa02c7930
NishwithX <165366126+NishwithX@users.noreply.github.com>
2024-10-04 15:13:08 +0530

    Update neko.js

--- neko.js
+++ neko.js
@@ -143,7 +143,7 @@
       nekoEl.style.height = "32px";
       nekoEl.style.position = "fixed";
       nekoEl.style.pointerEvents = "none";
-      nekoEl.style.backgroundImage = "url('')";
+      nekoEl.style.backgroundImage = "url('https://raw.githubusercontent.com/NishwithX/nekos/refs/heads/main/oneko.gif')";
       nekoEl.style.imageRendering = "pixelated";
       nekoEl.style.left = `${nekoPosX - 16}px`;
       nekoEl.style.top = `${nekoPosY - 16}px`;
